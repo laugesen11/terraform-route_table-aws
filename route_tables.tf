55e4f2c06b55a9ac82fb160d632af333ed132a01
Larry <laugesen11@gmail.com>
2022-09-16 06:27:46 -0500

    Updating route_tables

--- route_tables.tf
+++ route_tables.tf
@@ -8,7 +8,7 @@ locals {
     for item in var.route_tables: item.name => { 
       #If vpc_name_or_id is set to a VPC name in this module, we use that VPC id. If not, we assume this is an external VPC id
       "vpc_id"           = lookup(var.vpcs,item.vpc,null) != null ? var.vpcs[item.vpc].vpc.id : item.vpc
-      "propagating_vgws" = lookup(item.options,"propagating_vgws",null)
+      "propagating_vgws" = lookup(item.options,"propagating_vgws",[])
       "tags"             = lookup(item.options,"tags",null) == null ? {} : {
                              for tag in split(",",item.options["tags"]):
                                element(split("=",tag),0) => element(split("=",tag),1)
